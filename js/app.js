131144b01c35965669f2c05ebf02f3e1969eb18c
juantobares4 <j.tobares@itecriocuarto.org.ar>
2026-06-08 21:05:31 -0300

    add card number input validation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
-console.log('Hola mundo...');
+const formCardNumber = document.getElementById('inputCardNumber');
+const cardNumberText = document.getElementById('cardNumber');
+
+const handleFormDataInput = () => {
+  const regex = /[a-zA-Z]/;
+
+  formCardNumber.addEventListener('input', (event) => {
+    if (regex.test(event.target.value)) return;
+
+    const inputLength = event.target.value.length;
+    let numberCard = event.target.value;
+    
+    cardNumberText.textContent = numberCard;
+    
+    if (inputLength === 4 || inputLength === 9 || inputLength === 14) {
+      event.target.value = numberCard + ' ';
+      cardNumberText.textContent = numberCard + ' ';
+
+    };
+
+    
+  });
+
+};
+
+const handleFormDataSubmit = () => {
+
+
+};
+
+const main = () => {
+  handleFormDataInput();
+
+};
+
+main();
